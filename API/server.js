29dc51321b305ded9efa6d444924e6c91ad110a7
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-01-01 11:30:07 +0500

    DB Connection

--- API/server.js
+++ API/server.js
@@ -1,8 +1,12 @@
-const app=require('./app')
+const mongoose = require('mongoose')
+const app = require('./app')
 
 const dotenv = require('dotenv')
 dotenv.config({
     path: './config.env'
 });
 
-app.listen(3000, ()=>console.log('listening at port 5000.'))
+mongoose.connect(process.env.URI)
+    .then(() => console.log('DB connection successful!'));
+
+app.listen(3000, () => console.log('listening at port 5000.'))
