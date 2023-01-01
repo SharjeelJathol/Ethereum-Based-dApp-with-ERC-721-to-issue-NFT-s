41e378f50cfa5bfbbda5e905480db6d80da13e2d
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-01-01 11:11:26 +0500

    Body Parser
    To parse the body or request and add the data in the req variable at req.body and req.cookies

--- API/app.js
+++ API/app.js
@@ -7,6 +7,9 @@ app.use(cors({
     origin: ['http://localhost:3000', '*'],
     credentials: true
 }));
+/*Parse the body of request and add the data in req variable*/
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 // app.listen(3000, ()=>console.log('listening at port 5000.'))
 module.exports = app;
