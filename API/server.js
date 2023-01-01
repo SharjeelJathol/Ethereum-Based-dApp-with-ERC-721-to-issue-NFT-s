7f543ccfc239c5f5eec79e65a676c2a2d04b9dd5
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-01-01 11:52:34 +0500

    DB Connection

--- API/server.js
+++ API/server.js
@@ -6,7 +6,11 @@ dotenv.config({
     path: './config.env'
 });
 
-mongoose.connect(process.env.URI)
-    .then(() => console.log('DB connection successful!'));
+mongoose.set('strictQuery', 'false')
+mongoose.connect(process.env.URI,{
+    useNewUrlParser: true,
+})
+.then(() => console.log('DB connection successful!'))
+.catch(err=>console.log(err))
 
 app.listen(3000, () => console.log('listening at port 5000.'))
