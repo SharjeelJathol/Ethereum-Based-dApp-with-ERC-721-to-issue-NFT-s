b87399e768bdba9a98933cb16663fd179c0bac46
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-01-01 11:17:38 +0500

    NFT Routes
    Handle all routes/requests related to NFTs

--- API/app.js
+++ API/app.js
@@ -2,6 +2,8 @@ const express=require('express')
 const app=express();
 const cors=require('cors')
 
+const nftRoutes=require('./routes/nftRoutes')
+
 //middlewares
 app.use(cors({
     origin: ['http://localhost:3000', '*'],
@@ -11,6 +13,10 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+//Routes
+app.use('/NFT', nftRoutes)
+
+//All unhandled requests
 app.all('*', (req, res)=>res.send('Invalid Request'))
 // app.listen(3000, ()=>console.log('listening at port 5000.'))
 module.exports = app;
