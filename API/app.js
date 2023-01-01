59c7aa25e673a13e43d002198348f2010119aa62
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-01-01 10:39:03 +0500

    cors middleware
    To handle http requests across any platform/URL particularly from the same device.

--- API/app.js
+++ API/app.js
@@ -1,5 +1,12 @@
 const express=require('express')
 const app=express();
+const cors=require('cors')
+
+//middlewares
+app.use(cors({
+    origin: ['http://localhost:3000', '*'],
+    credentials: true
+}));
 
 // app.listen(3000, ()=>console.log('listening at port 5000.'))
 module.exports = app;
