a91d3d6a3144c40b78ac62d37c21f375506bcd94
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-03-07 01:45:54 +0500

    NFTs owned by requester

--- API/controllers/nftController.js
+++ API/controllers/nftController.js
@@ -14,3 +14,18 @@ exports.allNFTs = async (req, res) => {
     // res.send('None')
 }
 
+exports.myOwnedNFTs=async (req, res)=>{
+    try {
+        let response = await NFT.find({owner:req.query.public_address});
+        if (response) {
+            console.log(response)
+            res.json(response)
+        } else
+            res.send('Failed')
+
+    } catch (err) {
+        console.log(err)
+        res.status(404).send('Error')
+    }
+}
+
