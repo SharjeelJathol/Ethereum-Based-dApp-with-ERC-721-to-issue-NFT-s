f6f94268a1bbb23535134335aac2ee194b5f74af
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-03-07 01:47:33 +0500

    Save file on server

--- API/controllers/nftController.js
+++ API/controllers/nftController.js
@@ -44,3 +44,17 @@ exports.myMintedNFTs=async (req, res)=>{
     }
 }
 
+
+exports.uploadNFTPhoto = multer({ storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'NFTs')
+    },
+    filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    const name=file.fieldname +'-' + uniqueSuffix+`.${file.mimetype.split('/')[1]}`
+    cb(null, name)
+    req.body.url=name
+}
+})
+}).single('NFT')
+
